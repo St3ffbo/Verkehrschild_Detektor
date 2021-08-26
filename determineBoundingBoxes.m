e4769c2a5adc853f4a8647b3ad9aa1547ca3fbca
student <p.student@isam-ag.de>
2021-08-26 12:42:21 +0200

    Fixed enlargement of bounding boxes

--- determineBoundingBoxes.m
+++ determineBoundingBoxes.m
@@ -5,8 +5,13 @@ bw_color_mask_relevant_areas_img_props = regionprops(bw_color_mask_relevant_area
 bw_color_mask_relevant_areas_bb = reshape(extractfield(bw_color_mask_relevant_areas_img_props, 'BoundingBox'), 4, [])';
 
 %% Enlarge bounding boxes by 10%.
-bounding_boxes = [bw_color_mask_relevant_areas_bb(:,1)-bw_color_mask_relevant_areas_bb(:,1).*0.1...
-        bw_color_mask_relevant_areas_bb(:,2)-bw_color_mask_relevant_areas_bb(:,2).*0.1...
-        bw_color_mask_relevant_areas_bb(:,3)+2*bw_color_mask_relevant_areas_bb(:,3).*0.1...
-        bw_color_mask_relevant_areas_bb(:,4)+2*bw_color_mask_relevant_areas_bb(:,4).*0.1];
+scale = 1.1;
+bounding_boxes = zeros(size(bw_color_mask_relevant_areas_bb, 1), 4);
+
+% Adjust the lower left corner.
+bounding_boxes(:, 1:2) = bw_color_mask_relevant_areas_bb(:, 1:2) - bw_color_mask_relevant_areas_bb(:, 3:4) * 0.5 * (scale -1);
+
+% Adjust the width and the height.
+bounding_boxes(:, 3:4) = bw_color_mask_relevant_areas_bb(:, 3:4) * scale;
+
 end
