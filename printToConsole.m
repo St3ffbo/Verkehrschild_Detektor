4420813908e932415b3cec2dade19d7480c902da
student <p.student@isam-ag.de>
2021-09-10 17:59:02 +0200

    Added debug outputs and corresponding function

---
+++ printToConsole.m
@@ -0,0 +1,11 @@
+% Prints a message to the console if the debg flag is set.
+% Input: The message to print and the flag.
+
+function printToConsole(message, debug)
+
+if debug
+    disp(message);
+end 
+
+end
+    
--- trafficSignDetection.m
+++ trafficSignDetection.m
@@ -1,6 +1,6 @@
 % Detects traffic signs that have the given color on a given image.
 % Input: The image to detect traffic signs on, the color of the traffic
-% signs to look for and whether debug plots and message should be shown.
+% signs to look for and whether debug_mode plots and message should be shown.
 % Output: Detected traffic signs as instances of the AbstractTrafficSign
 % class.
 
@@ -12,13 +12,14 @@ function abstract_traffic_signs = trafficSignDetection(image, color, debug_mode)
     % Check whether any pixel has been masked by the color mask. If not,
     % return immediately.
     if ~any(bw_color_mask, 'all')
+        printToConsole(append('No ', string(color), 'area(s) found.'), debug_mode);
         abstract_traffic_signs = {};
         return
     end
     
     % Plot color masked image if desired.
     if debug_mode
-        figure('Name', strcat('Color masked image (',string(color),')'));
+        figure('Name', append('Color masked image (',string(color),')'));
         imshow(bw_color_mask);
     end
 
@@ -28,13 +29,14 @@ function abstract_traffic_signs = trafficSignDetection(image, color, debug_mode)
     % Check whether any relevant areas were found and return immediately if
     % not.
     if isempty(bw_color_mask_relevant_areas)
+        printToConsole(append(string(color), ' pipeline: No relevant area(s) found.'), debug_mode);
         abstract_traffic_signs = {};
         return
     end
     
     % Plot relevant areas if desired.
     if debug_mode
-        figure('Name', strcat('Relevant areas (',string(color),')'));
+        figure('Name', append('Relevant areas (',string(color),')'));
         imshow(bw_color_mask_relevant_areas);
     end
        
@@ -43,9 +45,11 @@ function abstract_traffic_signs = trafficSignDetection(image, color, debug_mode)
     bounding_boxes = determineBoundingBoxes(bw_color_mask_relevant_areas, color);
     [cropped_images_original, cropped_images_bw] = cropImage(image, bw_color_mask_relevant_areas, bounding_boxes);
     
+    printToConsole(append(string(color), ' pipeline: Cropped ', num2str(size(bounding_boxes, 1)), ' area(s) from image.'), debug_mode);
+    
     % Plot all cropped and black-white cropped images in one montage if desired.
     if debug_mode
-        figure('Name', strcat('Relevant area(s) cropped from image (',string(color),')'));
+        figure('Name', append('Relevant area(s) cropped from image (',string(color),')'));
         montage([cropped_images_original cropped_images_bw], 'Size', [2 size(cropped_images_original, 2)]);
     end
 
@@ -54,7 +58,7 @@ function abstract_traffic_signs = trafficSignDetection(image, color, debug_mode)
      
      % Plot all cropped and finetuned images in one montage if desired.
      if debug_mode
-        figure('Name', strcat('Finetuned cropped images (',string(color),')'));
+        figure('Name', append('Finetuned cropped images (',string(color),')'));
         montage([cropped_images_original cropped_images_bw_finetuned], 'Size', [2 size(cropped_images_original, 2)]);
      end
 
@@ -63,11 +67,11 @@ function abstract_traffic_signs = trafficSignDetection(image, color, debug_mode)
     
     % Plot all distance plots in one subplot if desired.
     if debug_mode
-        figure('Name', strcat('Distance plot for each relevant area (',string(color),')'));        
+        figure('Name', append('Distance plot for each relevant area (',string(color),')'));        
         for image_index = 1: length(distances_to_outlines)
             subplot(length(distances_to_outlines), 1, image_index);
             plot(distances_to_outlines{image_index});
-            title(strcat('Distance plot of relevant area no. ', num2str(image_index)));
+            title(append('Distance plot of relevant area no. ', num2str(image_index)));
         end
     end
 
@@ -76,13 +80,13 @@ function abstract_traffic_signs = trafficSignDetection(image, color, debug_mode)
 
     % Plot FFT of outline distances if desired in one subplot.
     if debug_mode
-        figure('Name', strcat('FFTs of outline distances (',string(color),')'));
+        figure('Name', append('FFTs of outline distances (',string(color),')'));
         number_images = length(fft_arrays);
         for image_index = 1:number_images
             subplot(number_images, 1, image_index);           
             f = (2:10)-1;        
             stem(f,fft_arrays{image_index});
-            title(strcat('FFT of distance plot of relevant area no. ', num2str(image_index)));   
+            title(append('FFT of distance plot of relevant area no. ', num2str(image_index)));   
         end
     end
 
